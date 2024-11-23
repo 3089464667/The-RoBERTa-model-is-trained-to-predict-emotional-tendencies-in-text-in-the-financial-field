5394323df12ea8f004d97ff1a2126c1ce02fdb65
3089464667 <112289893+3089464667@users.noreply.github.com>
2024-11-23 23:50:27 +0800

    Update script.js

--- static/script.js
+++ static/script.js
@@ -10,11 +10,9 @@ async function getSentiment(text) {
     const result = await response.json();
     console.log('Market judgment:', result.predicted_class);
 
-    // 在页面上显示结果
     document.getElementById('result').innerText = 'Predicted Sentiment Class: ' + result.market_judgment;
 }
 
-// 当用户点击按钮时调用该函数
 document.getElementById('submitButton').addEventListener('click', () => {
     const userInput = document.getElementById('textInput').value;
     getSentiment(userInput);
